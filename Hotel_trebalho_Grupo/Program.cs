eced4617d937fd986a5dd0b7372ec3fca15577e4
Exception-Unhandled <micaelamarimalbuquerque@gmail.com>
2025-02-06 22:04:16 +0000

    alt Hotel

--- Hotel_trebalho_Grupo/Program.cs
+++ Hotel_trebalho_Grupo/Program.cs
@@ -20,22 +20,33 @@ res[0].nome = nomes[nindex--];
 
 //Inicio
 
+
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("           TP 1 - modulo 7            ");
+Console.WriteLine("          Gestão de Reservas          ");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("--------------------------------------");
+Console.WriteLine("(Pressione ENTER para entrar no programa!)");
+Console.ReadLine();
+
 while ( op != 0 )
 {
     Console.Clear();
 
     Console.WriteLine("--------------------------------------");
     Console.WriteLine("--------------------------------------");
-    Console.WriteLine("           TP 1 - modulo 7            ");
-    Console.WriteLine("          Gestão de Quartos           ");
+    Console.WriteLine("         Bem-vindo ao Hotel!          ");
+    Console.WriteLine("         O que deseja fazer?          ");
+    Console.WriteLine("--------------------------------------");
     Console.WriteLine(" 1- Carregar reserva                  ");
     Console.WriteLine(" 2- Inserir nova reserva              ");
     Console.WriteLine(" 3- Pesquisar reservas                ");
-    Console.WriteLine(" 4- Mostrar todas as reservas         ");
+    Console.WriteLine(" 4- Mostrar reservas preenchidas      ");
     Console.WriteLine(" 5- Alterar dados da reserva          ");
     Console.WriteLine(" 6- Exportar reservas                 ");
     Console.WriteLine(" 7- Gerar reserva                     ");
-    Console.WriteLine(" 8- Nº de quartos livres              ");
+    Console.WriteLine(" 8- Nº de reservas livres             ");
     Console.WriteLine(" 9- Mostrar preços de quarto          ");
     Console.WriteLine(" 0- Sair                              ");
     Console.WriteLine("--------------------------------------");
@@ -45,14 +56,18 @@ while ( op != 0 )
     Console.WriteLine("Introduza a opção");
     op = Convert.ToInt32(Console.ReadLine());
 
+    // Carregar reserva
+
     if( op == 1)
     {
 
     }
 
+    // Inserir nova reserva
+
     if (op == 2)
     {
-        int disp = check(res);
+        int disp = checkV(res);
         if( disp != -1  )
         {
             Console.WriteLine("Introduza o nome da reserva:");
@@ -76,8 +91,13 @@ while ( op != 0 )
         }
         else
             Console.WriteLine("Não existem reservas disponíveis! (Limpe alguma reserva para armazenar uma nova)");
-       
+
+        Console.WriteLine("Reserva criada com sucesso! \n \r(Pressione ENTER para voltar ao menu)");
+        Console.ReadLine();
+
     }
+
+    // Pesquisar reserva
 
     if ( op == 3)
     {
@@ -104,8 +124,6 @@ while ( op != 0 )
                 if (quarto == res[i].Nquarto)
                     Dados(res[i]);
             }
-            Console.WriteLine("(Pressione ENTER para voltar ao menu)");
-            Console.ReadLine();
         }
         else if (op == 2)
         {
@@ -117,22 +135,41 @@ while ( op != 0 )
                 Dados(res[index]);
             }
             else
-                Console.WriteLine("A reserva com este numero não existe");
+                Console.WriteLine("A reserva com este número não existe");
         }
         else
             Console.WriteLine("Operação inválida");
+
+        Console.WriteLine("(Pressione ENTER para voltar ao menu)");
+        Console.ReadLine();
     }
+
+    // Mostrar todas as reservas
 
     if( op == 4)
     {
+        bool disp = false;
+
         for (int i = 0; i < res.Length; i++)
         {
             if (res[i].Nquarto != 0)
-                Console.WriteLine($"{res[i].Nquarto} {res[i].nome} {res[i].apelido} {res[i].Npessoa} {res[i].nTele} {res[i].preco}");
+            {
+                disp = true;
+                Dados(res[i]);
+                Console.WriteLine();
+            }
         }
 
-        Console.Clear();
+        if (disp == false)
+        {
+            Console.WriteLine("Ainda não existem reservas preenchidas!");
+        }
+
+        Console.WriteLine("(Pressione ENTER para voltar ao menu)");
+        Console.ReadLine();
     }
+
+    // Alterar dados da reserva
 
     if(op == 5)
     {
@@ -144,40 +181,53 @@ while ( op != 0 )
 
         if (op == 1)
         {
+            bool disp = false;
+
             Console.WriteLine("Reservas disponíveis:");
 
             for (int i = 0; i < res.Length; i++)
             {
+                disp = true;
                 if (res[i].Nquarto != 0)
                     Console.Write($"{res[i].Nquarto} ");
             }
 
-            Console.WriteLine();
-
-            Console.WriteLine("Introduza o número do quarto que quer alterar a reserva: ");
-            int quarto = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 0; i < res.Length; i++)
+            if (disp == false)
+                Console.WriteLine("Não existem reservas criadas!");
+            else
             {
-                int index = pesquisa(res, quarto);
+                disp = false;
+                Console.WriteLine();
 
-                if (index != -1)
+                Console.WriteLine("Introduza o número do quarto que quer alterar a reserva: ");
+                int quarto = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = 0; i < res.Length; i++)
                 {
-                    Console.WriteLine("Introduza um novo nome");
-                    res[index].nome = Console.ReadLine();
+                    int index = pesquisa(res, quarto);
 
-                    Console.WriteLine("Introduza um novo apelido");
-                    res[index].apelido = Console.ReadLine();
+                    if (index != -1)
+                    {
+                        disp = true;
+                        Console.WriteLine("Introduza um novo nome");
+                        res[index].nome = Console.ReadLine();
 
-                    Console.WriteLine("Introduza um novo número de telemovel");
-                    res[index].nTele = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Introduza um novo apelido");
+                        res[index].apelido = Console.ReadLine();
 
-                    Console.WriteLine("Introduza uma nova quantidade de pessoas");
-                    res[index].Npessoa = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Introduza um novo número de telemovel");
+                        res[index].nTele = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Introduza uma nova quantidade de pessoas");
+                        res[index].Npessoa = Convert.ToInt32(Console.ReadLine());
+                    }
                 }
 
-                else
+                if (disp == false)
                     Console.WriteLine("A reserva com este numero não existe");
+
+                Console.WriteLine("(Pressione ENTER para voltar ao menu)");
+                Console.ReadLine();
             }
         }
 
@@ -209,10 +259,14 @@ while ( op != 0 )
 
     }
 
+    // Exportar reservas
+
     if (op == 6)
     {
 
     }
+
+    // Gerar reserva
 
     if(op ==7)
     {
@@ -221,19 +275,56 @@ while ( op != 0 )
         Console.ReadLine();
     }
 
+    // Reservas disponíveis
+
     if (op == 8)
     {
         int contar = 0;
         for (int i = 0;i < res.Length;i++)
         {
-            if (res[i].nome == (res[i].nome=string.Empty))
+            if (res[i].Nquarto == 0)
             {
                 contar= contar+1 ;
             }
         }
-        Console.WriteLine($" A {contar} quartos disponíveis ");
+        Console.WriteLine($"Existem {contar} reservas disponíveis ");
+        Console.WriteLine("(Pressione ENTER para voltar ao menu)");
         Console.ReadLine();
-        Console.Clear();
+    }
+
+    //Sair
+
+    if (op == 0)
+    {
+        bool valido = false;
+
+        while (valido == false)
+        {
+            Console.Clear();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("          Deseja mesmo sair?          ");
+            Console.WriteLine(" 1 - Sim \n \r 2 - Não");
+            Console.WriteLine("--------------------------------------");
+            op = Convert.ToInt32(Console.ReadLine());
+
+            if (op == 1)
+                return;
+            else if (op == 2)
+                valido = true;
+            else
+            {
+                Console.WriteLine("Opção inválida!");
+                Console.WriteLine("(Pressione ENTER para voltar a tentar)");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    if (op != 0 && op != 1 && op != 2 && op != 3 && op != 4 && op != 5 && op != 6 && op != 7 && op != 8)
+    {
+        Console.WriteLine("Opção inválida!");
+        Console.WriteLine("(Pressione ENTER para voltar a tentar)");
+        Console.ReadLine();
     }
 
 }
@@ -266,11 +357,11 @@ static void Dados(reserva a)
 
 }
 
-static int check(reserva[] a)
+static int checkV(reserva[] a)
 {
     for (int i = 0;i < a.Length; i++)
     {
-        if (a[i].nome == string.Empty)
+        if (a[i].Nquarto == 0)
         {
             return i;
         } 
